b823926fbb9468da6481e11fd9ba217c002fea63
123jian <1048124167@qq.com>
2014-12-14 16:25:59 +0800

    a

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -1,12 +1,20 @@
 <?php
 
 namespace frontend\controllers;
-
+use app\models\KekeWitkeyArticle;
+use app\models\KekeWitkeyArticleCategory;
 class ArticleController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->renderpartial('index');
+        $model=new query();
+        $model->from(['keke_witkey_model','keke_witkey_task'])->where(
+                "keke_witkey_task.model_id=keke_witkey_model.model_id and keke_witkey_task.model_id=1 order by task_id desc")->all();
+
+        $pages = new Pagination(['totalCount'=>$model->count(),'pageSize'=>5]);
+        $data=$model->offset($pages->offset)->limit($pages->limit)->all();
+               
+        return $this->renderpartial('index',['arr'=>$data,'pages'=>$pages]);
     }
 
 }
--- frontend/controllers/CaseController.php
+++ frontend/controllers/CaseController.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace frontend\controllers;
-
+use app\models\KekeWitkeyCase;
 class CaseController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->renderpartial('index');
+       $arr = KekeWitkeyCase::find()->all(); 
+        return $this->renderpartial('index',['arr'=>$arr]);
+    }
+    public function actionSelect()
+    {
+       $arr = KekeWitkeyCase::find()->where(['case_id' => $_GET['sid']])->all(); 
+        return $this->renderpartial('select',['arr'=>$arr]);
     }
 
 }
